5b870a30c7c934b67a3a51af82d263979150e24b
Chaplin16 <chaplin16@orange.fr>
2023-03-12 00:31:38 +0100

    settings animations

--- JS/animImg.js
+++ JS/animImg.js
@@ -1,10 +1,23 @@
 const jump = document.querySelector(".jump");
+const turn = document.querySelector(".turn");
 
-//animate in scroll of 800px for img in  the text in index.html
+//animate in scroll of 1000px for img in  the text in index.html AND for logo in footer
 window.addEventListener("scroll", () => {
-    if (window.scrollY < 1000) {
+    if (window.scrollY >= 450) {
       jump.classList.add("jumpImg");  
+      turn.classList.add("turn");  
     } else {
       jump.classList.remove("jumpImg");
+      turn.classList.remove("turn");
+    }
+  });
+
+
+  //animate in scroll of 1300px for logo in footer
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= 1700 ) {
+      turn.classList.add("turn");  
+    } else {
+      turn.classList.remove("turn");
     }
   });
