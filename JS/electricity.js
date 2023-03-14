c7a0a76ed7edf70ab1a782240cf6dffb5fe55470
Chaplin16 <chaplin16@orange.fr>
2023-03-14 11:23:25 +0100

    rectif nav in page electricity

--- JS/electricity.js
+++ JS/electricity.js
@@ -1,62 +1,62 @@
 const electricity = [
     {
         id:1,
-        link:"images/card-electricity/raccordement-electricite-gard-herault.jpg",
-        alt:""
+        link:"../images/card-electricity/raccordement-electricite-gard-herault.jpg",
+        alt:"fgqsdg"
     },
     {
         id:2,
-        link:"images/card-electricity/installation-lumiere-montpellier-nimes-aimargues.jpg",
+        link:"../images/card-electricity/installation-lumiere-montpellier-nimes-aimargues.jpg",
         alt:""
     },
     {
         id:3,
-        link:"images/card-electricity/pose-prise-courant-electricité-gard-herault.jpg",
+        link:"../images/card-electricity/pose-prise-courant-electricité-gard-herault.jpg",
         alt:""
     },
     {
         id:4,
-        link:"images/card-electricity/circuit-electrique-aimargues-gard-herault.jpg",
+        link:"../images/card-electricity/circuit-electrique-aimargues-gard-herault.jpg",
         alt:""
     },
     {
         id:5,
-        link:"images/card-electricity/lampes-electricite-lumiere-montpellier.jpg",
+        link:"../images/card-electricity/lampes-electricite-lumiere-montpellier.jpg",
         alt:""
     },
     {
         id:6,
-        link:"images/card-electricity/travaux-electricite-aimargues-nimes-montpellier-home-art.jpg",
+        link:"../images/card-electricity/travaux-electricite-aimargues-nimes-montpellier-home-art.jpg",
         alt:""
     },
     {
         id:7,
-        link:"images/card-electricity/testeur-electricite-climatisation.jpg",
+        link:"../images/card-electricity/testeur-electricite-climatisation.jpg",
         alt:""
     },
     {
         id:8,
-        link:"images/card-electricity/raccordement-electrique-climatisation-aimargues-nimes-montpellier.jpg",
+        link:"../images/card-electricity/raccordement-electrique-climatisation-aimargues-nimes-montpellier.jpg",
         alt:""
     },
     {
         id:9,
-        link:"images/card-electricity/prise-courant-raccordement-montpellier.jpg",
+        link:"../images/card-electricity/prise-courant-raccordement-montpellier.jpg",
         alt:""
     },
     {
         id:10,
-        link:"images/card-electricity/installation-tableau-electrique-aimargues.jpg",
+        link:"../images/card-electricity/installation-tableau-electrique-aimargues.jpg",
         alt:""
     },
     {
         id:11,
-        link:"images/card-electricity/travaux-electricite-aimargues-gard.jpg",
+        link:"../images/card-electricity/travaux-electricite-aimargues-gard.jpg",
         alt:""
     },
     {
         id:12,
-        link:"images/card-electricity/raccordement-courant-electricite-aimargues.jpg",
+        link:"../images/card-electricity/raccordement-courant-electricite-aimargues.jpg",
         alt:""
     }
 ]
--- JS/galleryElectricity.js
+++ JS/galleryElectricity.js
@@ -1,10 +1,30 @@
 const galleryElectricity = document.getElementById("gallery");
 
-for (const element of electricity) {
-    console.log(element);
-    galleryElectricity.innerHTML +=
-    `<li>
-        <img src=${element.link} alt=${element.alt} id=${element.id} />
-    </li>`
-  }
-  
+function slider(selector, data) {
+    let element = document.querySelector(selector);
+    data.forEach(function(element) {
+        element.innerHTML += 
+        ` <li>
+              <img src="${element.link}" alt="${element.alt}" id="${element.id}" />
+          </li>`;
+    }); 
+
+  $(`.slider-for`).slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+}
+      
+
+galleryElectricity(".slider", electricity)
