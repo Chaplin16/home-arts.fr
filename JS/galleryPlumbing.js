9cd5e6af5da9c971755bf582670301f60973a97e
Chaplin16 <chaplin16@orange.fr>
2023-03-15 18:38:20 +0100

    hover in card plumbing

--- JS/galleryPlumbing.js
+++ JS/galleryPlumbing.js
@@ -2,7 +2,7 @@ const galleryPlumbing = document.getElementById("galleryPlumbing");
 
 for (const element of plumbing) {
     galleryPlumbing.innerHTML +=`
-    <li class="list-unstyled" >
+    <li class="list-unstyled overflow-hidden" >
         <img data-kt-rotate="true" src=${element.link}  id="${element.id}" alt=${element.alt}/>
     </li>
     `
