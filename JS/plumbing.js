7c49a08c7f746d297daadcebc90ed1f896034a68
Chaplin16 <chaplin16@orange.fr>
2023-03-15 17:10:25 +0100

    text in page plumbing.html

--- JS/plumbing.js
+++ JS/plumbing.js
@@ -1,6 +1,47 @@
 const plumbing = [
     {
         alt:1,
-        link:
+        link:"../images/card-plumbing/installation-plomberie-baignoire-gard-herault.jpg",
+        alt:""
+    },
+    {
+        alt:2,
+        link:"../images/card-plumbing/depannage-fuite-evier-plomberie-aimargues-gard-herault.jpg",
+        alt:""
+    },
+    {
+        alt:3,
+        link:"../images/card-plumbing/circuit-plomberie-eau-chaude-froide.jpg",
+        alt:""
+    },
+    {
+        alt:4,
+        link:"../images/card-plumbing/depannage-reparation-installation-plomberie-baignoire.jpg",
+        alt:""
+    },
+    {
+        alt:5,
+        link:"../images/card-plumbing/installation-plomberie-baignoire-aimargues.jpg",
+        alt:""
+    },
+    {
+        alt:6,
+        link:"../images/card-plumbing/circuit-plomberie-aimargues-gard-hearult.jpg",
+        alt:""
+    },
+    {
+        alt:7,
+        link:"../images/card-plumbing/douche-plomberie-aimargues.jpg",
+        alt:""
+    },
+    {
+        alt:8,
+        link:"../images/card-plumbing/evier-plomberie-fuite-reparé-aimargues.jpg",
+        alt:""
+    },
+    {
+        alt:9,
+        link:"../images/card-plumbing/installation-plomberie-baignoire-gard-herault.jpg",
+        alt:""
     }
 ]
